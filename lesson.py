213552bf06c935a04a70a72ebb1166275f2ad2c9
serhii_sokolovskyi <sokolovskiy0103@gmail.com>
2026-05-18 09:24:36 +0300

    Conversdation

--- lesson.py
+++ lesson.py
@@ -1,6 +1,10 @@
-import requests
 import sqlite3
 
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+from langchain_core.tools import tool
+
+load_dotenv()
 
 class ChatDatabase:
     def load_messages(self):
@@ -18,34 +22,58 @@ class ChatDatabase:
         conn.commit()
         conn.close()
 
+BOMZ_PROMPT = """Ти веселий бомж Толік. Постійно травиш якісь приколи, іноді відповідаєш похабно і хамовито. Саботуєш завдання з причини алкогольного спяніння. Але загалом радужний і добряк. Якщо до тебе гарно звертаються то ти чемний і слухняний."""
+POLICE_PROMPT = """Ти суворий поліцейський Ігнат який вирішив доколупатись до місцевого бомжа"""
+
+
+def swap_roles(messages: list[dict]) -> list[dict]:
+    role_map = {"user": "assistant", "assistant": "user"}
+    return [
+        {**msg, "role": role_map.get(msg["role"], msg["role"])}
+        for msg in messages
+    ]
+
+@tool
+def notate(record: str) -> str:
+    """
+    Зробити запис в свій нотатник
+    """
+    print('\033[35m', end='', flush=True)
+    print(record)
+    print('\033[0m', flush=True)
+
 
 def main():
     db = ChatDatabase()
-    messages = [{
-        "role": "system",
-        "content": """Ти веселий бомж Толік. Постійно травиш якісь приколи, іноді відповідаєш похабно і хамовито. Саботуєш завдання з причини алкогольного спяніння. Але загалом радужний і добряк. Якщо до тебе гарно звертаються то ти чемний і слухняний."""
-    }]
-    messages = messages + db.load_messages()
+    llm = init_chat_model(
+        base_url='http://127.0.0.1:1234/v1',
+        model='google/gemma-4-e4b',
+        model_provider='openai',
+        api_key='dgssd'
+    )
+    llm_police = llm.bind_tools([notate])
+    messages = ChatDatabase().load_messages()[-3:]
 
     while True:
-        user_message = input("\n You:")
-        messages.append({"role": "user", "content": user_message})
-        db.save_message("user", user_message)
+        result = ''
+        response = llm_police.stream([{"role": "system", "content": POLICE_PROMPT}] + messages)
+        print('\033[34m', end='', flush=True)
+        for message in response:
+            result += message.content
+            print(message.content, end='')
+        print('\033[0m', flush=True)
+        db.save_message("assistant", result)
+        messages.append({"role": "assistant", "content": result})
+        print('\n')
 
-        response = requests.request(
-            'POST',
-            'http://127.0.0.1:1234/v1/chat/completions',
-            json={
-                "messages": messages,
-                "model": "google/gemma-4-e4b"
-            }
-        )
-        response_json = response.json()
-        ai_message = response_json["choices"][0]["message"]["content"]
-        messages.append({"role": "assistant", "content": ai_message})
-        db.save_message("assistant", ai_message)
-        print("\nAI: " + ai_message)
+        result = ''
+        response = llm.stream([{"role": "system", "content": BOMZ_PROMPT}] + swap_roles(messages))
 
+        for message in response:
+            result += message.content
+            print(message.content, end='')
+        db.save_message("user", result)
+        messages.append({"role": "user", "content": result})
 
 if __name__ == '__main__':
     main()
