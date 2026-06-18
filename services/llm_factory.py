7da5038371b8d07adbc3d07a70ba0d9dd19bf279
serhii_sokolovskyi <sokolovskiy0103@gmail.com>
2026-06-18 09:55:36 +0300

    Notebooks

--- services/llm_factory.py
+++ services/llm_factory.py
@@ -4,7 +4,7 @@ from langchain.chat_models import init_chat_model
 def get_llm():
     return init_chat_model(
         base_url="http://127.0.0.1:1234/v1",
-        model="google/gemma-4-26b-a4b",
+        model="google/gemma-4-26b-a4b-qat",
         model_provider="openai",
         api_key="lm-studio",
     )
